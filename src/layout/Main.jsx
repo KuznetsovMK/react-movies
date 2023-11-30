ee760b4ed88a531799b256fa49d541bcafcf7dda
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2023-11-30 21:53:11 +0300

    load movies

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,27 @@
-function Main() {
-    return <main className='container content'>
-        Hello from movies App
-    </main>
+import React from "react";
+import {Movies} from "../components/Movies";
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=3cc29541&s=matrix')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    render() {
+        const {movies} = this.state;
+
+        return <main className='container content'>
+            {
+                movies.length ? (<Movies movies={movies}/>) : <h3>Loading...</h3>
+            }
+
+        </main>
+    }
 }
+
 export {Main}
