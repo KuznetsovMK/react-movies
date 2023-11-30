cf04e4ce6f76a6c73458c6ec0471bce65143afd6
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2023-11-30 20:57:36 +0300

    add footer, main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import {Header} from "./layout/Header";
 import React from "react";
+import {Header} from "./layout/Header";
+import {Footer} from "./layout/Footer";
+import {Main} from "./layout/Main";
 
 function App() {
     return (
         <>
             <Header/>
+            <Main />
+            <Footer/>
         </>
     );
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
-    return <nav>
+    return <nav className="blue-grey darken-2">
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Logo</a>
+            <a href="#" className="brand-logo">Movies</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="!#">Repo</a></li>
             </ul>
