ba7c13b233108164612a27096665be32b7811db6
Кузнецов Михаил Владимирович <Kuznetsov.M@russianpost.ru>
2023-12-04 21:14:58 +0300

    added catch exception

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -17,7 +17,10 @@ class Main extends React.Component {
             .then((response) => response.json())
             .then((data) =>
                 this.setState({movies: data.Search, loading: false})
-            )
+            ).catch((err) => {
+            console.error(err);
+            this.setState({loading: false})
+        })
     }
 
     searchMovies = (str, type = 'all') => {
@@ -30,7 +33,10 @@ class Main extends React.Component {
             .then((response) => response.json())
             .then((data) =>
                 this.setState({movies: data.Search, loading: false})
-            );
+            ).catch((err) => {
+            console.error(err);
+            this.setState({loading: false})
+        });
     };
 
     render() {
